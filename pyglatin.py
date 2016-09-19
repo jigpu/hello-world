314b17c79fd2f92023e65a9eb662c4da33cad111
eringrace <erin.grace.writer@gmail.com>
2016-09-19 16:41:42 -0700

    Corrected issue with IF statement!
    Correct the issue with the IF statement and got some sweet pro-tips about IN. Adding a section to search for blends.

--- pyglatin.py
+++ pyglatin.py
@@ -6,13 +6,40 @@ original = raw_input("Enter a word: ")
 if len(original) > 0 and original.isalpha():
 	word = original.lower()
 	first = word[0]
-	if word[0] != "a" or "e" or "i" or "o" or "u":
-		new_word = word + first + pyg
-		new_word = new_word[1:]
-
-	else:
+	
+	if word[0] in "aeiou":
 		new_word = word + pyg
 		new_word = new_word[0:]
+
+	##elif word[0:1] == "bl"\
+	##	or "br"\
+	##	or "bw"\
+	##	or "ch"\
+	##	or "cl"\
+	##	or "cr"\
+	##	or "cs"\
+	##	or "cw"\
+	##	or "cz"\
+	##	or "dh"\
+	##	or "dj"\
+	##	or "dr"\
+	##	or "fl"\
+	##	or "fr"\
+	##	or "fw"\
+	##	or "gh"\
+	##	or "gl"\
+	##	or "gn"\
+	##	or "gr"\
+	##	or "gw"\
+	##	or "hr"\
+	##	or "jh"\
+	##	or "jr"\
+	##	or "jw"\
+	##	or "k
+	
+	else:
+		new_word = word + first + pyg
+		new_word = new_word[1:]
 
 	print "\n"
 	print new_word
