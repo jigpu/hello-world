4eecb3232a69528ba649baf09b8aaffb5915d541
eringrace <erin.grace.writer@gmail.com>
2016-10-08 15:07:59 -0700

    Fixed accommodation function!
    I finally figured out what was happening! Looks like when I tried to pass user input from the acc_error function back into the acc_var function, it wasn't being maintained. Not sure what that's about, but at least I know now to keep my eye out for problems when passing variables between functions.

--- japan-calculator.py
+++ japan-calculator.py
@@ -1,6 +1,6 @@
-
-''' Breakfast subtracts one meal, since flights from the US don't arrive in the morning.'''
-''' Dinners subtracts one meal, since flights to the US leave before dinner.'''
+####################### MEALS #######################
+# Breakfast subtracts one meal, since flights from the US don't arrive in the morning.
+# Dinners subtracts one meal, since flights to the US leave before dinner.
 
 def meals():
     breakfast = (750 * days) - 750 
@@ -11,6 +11,9 @@ def meals():
     meal_cost = breakfast + lunch + dinner
     return meal_cost
 
+####################### ACCOMODATION #######################
+# This section doesn't work right with exception output
+
 def accomodation(acc_var):
 	if acc_var == "1":
 		return 8500 * days
@@ -19,17 +22,13 @@ def accomodation(acc_var):
 	elif acc_var == "3": 
 		return 3000 * days
 	else:
-		print ("\nSorry, I wasn't clear. If you want to stay in a hotel, press 1; if you want to stay in a guest house, press 2; and if you want to stay in a hostel, press 3.")
-		acc_error()
+		acc_var = raw_input("\nSorry, I wasn't clear. If you want to stay in a hotel, press 1; if you want to stay in a guest house, press 2; and if you want to stay in a hostel, press 3.")
+		return accomodation(acc_var)
 
-def acc_error():
-	acc_var = raw_input("\nSo, then, what kind of place do you want to stay?")
-	accomodation(acc_var)
+####################### TRAVEL #######################
+
 
 days = int(raw_input("How many days do you want to stay in Japan?"))
-print "\nOkay, %s days! Let's see..." % (days)
 print "\nThat'll be about %s yen in meals..." % (meals())
-acc_var = raw_input("\nWhat kind of place do you want to stay? A hotel (1), a guest house (2), or a hostel (3)?")
+acc_var = raw_input("\nWhat kind of accomodations do you want?\n     If you want to stay in a hotel, press 1.\n     If you want to stay in a guest house, press 2.\n     If you want to stay in a hostel, press 3. \n")
 print "\nSo you'll spend about %s on accommodations..." % (accomodation(acc_var))
-
-
